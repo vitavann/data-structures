0b549b9602ea969e0664cfb4e4d422101ab1dee7
vitavann <vitavann@gmail.com>
2019-09-07 10:38:05 -0700

    Implemented linked list data structure

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,24 +4,50 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
-    // start at tail
-    //
+    // create new node
+    var node = Node(value);
+    // if tail does not exist
+    if (!list.tail) {
+      // set head & tail to same node
+      list.tail = node;
+      list.head = node;
+    } else {
+      // if list already exists
+      // set tail.next to node & tail to new node
+      list.tail.next = node;
+      list.tail = node;
+    }
   };
 
-  list.removeHead = function() {
+  list.removeHead = function(value) {
+    // create node
+    // console.log('head' , list.head);
+    // console.log('tail' , list.tail);
+    var node = Node(value);
+    list.head = list.tail;
+    // console.log('new head ', list.head);
+    return list.head.value;
   };
 
   list.contains = function(target) {
     // begin at head
     // loop over list
-    for (key of list) {
-      if(list.hasOwnProperty(target)) {
+    for (var key in list) {
+      // check if target matches current node
+      if (list.head.value === target) {
+        // return true
+        return true;
+      }
+      if (list.tail.value === target) {
         return true;
       }
     }
-      // if target matches current element
-        // return true
-      // else return false
+    // return false
+    return false;
+
+    console.log('list ', list);
+    console.log('tail ', list.tail.value);
+    console.log('head ', list.head.value);
   };
 
   return list;
